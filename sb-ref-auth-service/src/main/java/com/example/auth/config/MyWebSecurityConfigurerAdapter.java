86cac2dacd63a6b6f709a38bf9cf666113135545
Michael Wright <michaelwright@Michaels-iMac.local>
2019-01-12 15:04:56 -0500

    add sec adapter

--- sb-ref-auth-service/src/main/java/com/example/auth/config/MyWebSecurityConfigurerAdapter.java
+++ sb-ref-auth-service/src/main/java/com/example/auth/config/MyWebSecurityConfigurerAdapter.java
@@ -1,10 +1,12 @@
 package com.example.auth.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 public class MyWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
 	   @Override
