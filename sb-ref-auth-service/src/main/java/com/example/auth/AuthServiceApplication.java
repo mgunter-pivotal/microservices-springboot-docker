bec3dc83991afec8c6c28cc29770ebbff876d916
Michael Wright <michaelwright@Michaels-iMac.local>
2019-01-12 15:08:28 -0500

    put auth bean on app class

--- sb-ref-auth-service/src/main/java/com/example/auth/AuthServiceApplication.java
+++ sb-ref-auth-service/src/main/java/com/example/auth/AuthServiceApplication.java
@@ -3,6 +3,9 @@ package com.example.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -16,4 +19,9 @@ public class AuthServiceApplication extends WebSecurityConfigurerAdapter {
         SpringApplication.run(AuthServiceApplication.class, args);
     }
 
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+	   @Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+	       return super.authenticationManagerBean();
+	   }
 }
